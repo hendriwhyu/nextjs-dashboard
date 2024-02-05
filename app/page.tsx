596fb8e0bc8fe22f0b67c5ff5a653962571137ad
hendriwhyu <hendriwp123@gmail.com>
2024-02-05 16:20:26 +0700

    fix: style Image

--- app/page.tsx
+++ app/page.tsx
@@ -39,11 +39,12 @@ export default function Page() {
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
+          {/* Hero Images Desktop */}
           <Image
             src={'/hero-mobile.png'}
             width={560}
             height={620}
-            className='block md:hidden'
+            className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
